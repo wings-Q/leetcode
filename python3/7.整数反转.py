fae436f402488d42d858387574b853a0811ee109
wings-Q <wings@wings.localdomain>
2019-11-07 13:49:25 +0800

    7.py-2

--- python3/7.整数反转.py
+++ python3/7.整数反转.py
@@ -7,6 +7,27 @@
 # @lc code=start
 class Solution:
     def reverse(self, x: int) -> int:
-        
-# @lc code=end
+        L = []
+        i = 0
+        w = 10
+        y = 0
+        uni = False
+        if x<0:
+            x = -x
+            uni = True
+        while True:
+            L.append(x%w)
+            x = (x - L[i])/10
+            i = i + 1
+            if x < 0.5:
+                break
+        for l in L:
+            y = y*10 + l
+        if uni:
+            y = -y
+        if y<-2**31 or y>2**31-1:
+            return 0
+        else:
+            return int(y)
 
+# @lc code=end
