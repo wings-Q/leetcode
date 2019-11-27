0909226205080b465348bab10c34e04b826e952c
wings-Q <im.qipf@gmail.com>
2019-11-27 11:43:43 +0800

    9.py 判断正负

--- python3/9.回文数.py
+++ python3/9.回文数.py
@@ -7,6 +7,8 @@
 # @lc code=start
 class Solution:
     def isPalindrome(self, x: int) -> bool:
+        if x<0:
+            return False
         strx = str(x)
         if strx == strx[::-1]:
             return True
